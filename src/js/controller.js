0c7e989c6459cc0027032f8dfd495dea7fcd9d08
rakmate9812 <rakmate9812@gmail.com>
2023-03-27 18:09:11 +0200

    small-change-to-search-box

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,11 +1,2 @@
 import $ from "jquery";
-
-$(".default_option").click(function () {
-  $(".dropdown ul").addClass("active");
-});
-
-$(".dropdown ul li").click(function () {
-  var text = $(this).text();
-  $(".default_option").text(text);
-  $(".dropdown ul").removeClass("active");
-});
+console.log($);
