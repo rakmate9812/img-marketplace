e09d35c367f6a3c965e5ab2d3b0593753393ed01
rakmate9812 <rakmate9812@gmail.com>
2023-03-30 19:04:59 +0200

    testing-out-api

--- src/js/index/indexController.js
+++ src/js/index/indexController.js
@@ -2,6 +2,7 @@ import $ from "../../../node_modules/jquery/dist/jquery.min.js";
 import * as model from "./indexModel.js";
 import * as cfg from "../config.js";
 
-console.log($());
+// console.log($());
 
-console.log(model.getRandomImage(cfg.ACCESS_KEY));
+// model.getRandomImage(cfg.ACCESS_KEY);
+model.getImageCollection(cfg.ACCESS_KEY);
--- src/js/index/indexModel.js
+++ src/js/index/indexModel.js
@@ -1,5 +1,14 @@
-export const getRandomImage = function (accessToken) {
+// https://api.unsplash.com/search/photos?client_id=YOUR_ACCESS_KEY?page=1&query=mountains
+
+export const getRandomImage = async function (accessToken) {
   const url = `https://api.unsplash.com/photos/random/?client_id=${accessToken}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.urls.regular));
+};
+
+export const getImageCollection = function (accessToken) {
+  const url = `https://api.unsplash.com/search/photos/?client_id=${accessToken}&query=mountains&page=21&per_page=6`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => console.log(data));
