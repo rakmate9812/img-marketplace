bbe9a96ded2d7b0af1b7f41cc4a407720e88bcab
rakmate9812 <rakmate9812@gmail.com>
2023-04-01 18:29:27 +0200

    implementing-slider

--- src/js/index/indexController.js
+++ src/js/index/indexController.js
@@ -9,3 +9,33 @@ model.getData(
   "https://api.unsplash.com/search/photos/?query=mountains&page=21&per_page=6",
   cfg.ACCESS_KEY
 );
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 6,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
