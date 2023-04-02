a1e66e2d01de93013c51ba9210bb99194dbd737b
rakmate9812 <rakmate9812@gmail.com>
2023-04-02 11:20:19 +0200

    fixing-slider

--- src/js/index/indexController.js
+++ src/js/index/indexController.js
@@ -1,41 +1,13 @@
 import $ from "../../../node_modules/jquery/dist/jquery.min.js";
 import * as model from "./indexModel.js";
+import * as view from "./indexView.js";
 import * as cfg from "../config.js";
 
 // console.log($());
 
-// getting 6 images from unsplash
-model.getData(
-  "https://api.unsplash.com/search/photos/?query=mountains&page=21&per_page=6",
-  cfg.ACCESS_KEY
-);
+// model.getData(
+//   "https://api.unsplash.com/search/photos/?query=mountains&page=21&per_page=6",
+//   cfg.ACCESS_KEY
+// );
 
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 6,
-  spaceBetween: 25,
-  loop: true,
-  centerSlide: "true",
-  fade: "true",
-  grabCursor: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    950: {
-      slidesPerView: 3,
-    },
-  },
-});
+view.trendingSwiper();
---
+++ src/js/index/indexView.js
@@ -0,0 +1,30 @@
+export const trendingSwiper = function () {
+  new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    loop: false,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      650: {
+        slidesPerView: 4,
+      },
+      950: {
+        slidesPerView: 6,
+      },
+    },
+  });
+};
